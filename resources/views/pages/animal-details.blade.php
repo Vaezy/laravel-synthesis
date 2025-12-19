63182faf366801e8eeb77c948810489350a25e50
Vaezy <boy1.thoomas@gmail.com>
2025-12-19 17:06:04 +0100

    refactor: change animal detail page style

--- resources/views/pages/animal-details.blade.php
+++ resources/views/pages/animal-details.blade.php
@@ -3,5 +3,17 @@
 @section('title', $animal->name)
 
 @section('content')
-    <x-animal :animal="$animal" />
+<div class="animal-detail-container">
+    <h2 class="animal-detail-title">{{ $animal->name }}</h2>
+    <div class="animal-detail-content">
+        <div class="animal-detail-img">
+            <img src="{{ asset('images/' . $animal->photo) }}" alt="{{ $animal->name }}">
+        </div>
+        <div class="animal-detail-info">
+            <p>Espèce : {{ $animal->species }}</p>
+            <p>Âge : {{ $animal->age }} ans</p>
+            <p>{{ $animal->description }}</p>
+        </div>
+    </div>
+</div>
 @endsection
